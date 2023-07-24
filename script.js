e52f7886e5e41d52eb5e0196ae72e10794cb605a
MedBelk <belkadimohammedyoucef@gmail.com>
2023-07-24 12:42:44 +0100

    update

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-
 async function getAdvice() {
-    var response = await fetch("https://api.adviceslip.com/advice");
-    var data = await response.json();
-    console.log(data);
-    return data;
-}
-function displayAdvice(id, advice) {
-    var adviceId= document.getElementById('adviceId');
-    adviceId.innerHTML = `advice number: ${id}:;<br>your advice: ${advice}`;
-}
-
-const Btn = document.getElementById('submit');
-Btn.addEventListener('click', async () => {
     try {
-        const a = await getAdvice();
-        console.log(a);
-        displayAdvice(a.slip.id, a.slip.advice);
+      var response = await fetch("https://api.adviceslip.com/advice");
+      var data = await response.json();
+      return data;
     } catch (error) {
-        console.error('Error fetching data:', error);
+      throw new Error('Error fetching data:', error);
     }
-});
-
+  }
+  
+  function displayAdvice(id, advice) {
+    var adviceId = document.getElementById('adviceId');
+   adviceId.innerHTML = "";
+    adviceId.innerHTML = `ADVICE # ${id}`;
+    var adv = document.getElementById('advice');
+    adv.innerHTML = "";
+    adv.innerHTML = `"${advice}"`;
+  }
+  
+  const Btn = document.getElementById('dice');
+  Btn.addEventListener('click', async () => {
+    try {
+      const a = await getAdvice();
+      displayAdvice(a.slip.id, a.slip.advice);
+    console.log(a);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      
+    }
+  });
+  
